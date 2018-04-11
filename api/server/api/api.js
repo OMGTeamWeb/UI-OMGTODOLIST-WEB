9e20ece59999df42fb01fa16699488262a65ec0b
Alberto Hernandez <beto.he7@gmail.com>
2018-04-10 19:25:39 -0500

    limpiar api

--- api/server/api/api.js
+++ api/server/api/api.js
@@ -3,14 +3,5 @@ var router = require('express').Router();
 // api router will mount other routers
 // for all our resources
 router.use('/player', require('./player/playerRoutes'));
-router.use('/teacher', require('./teacher/teacherRoutes'));
-router.use('/student', require('./student/studentRoutes'));
-router.use('/group', require('./group/groupRoutes'));
-
-router.use('/bimester', require('./bimester/bimesterRoutes'));
-router.use('/topic', require('./topic/topicRoutes'));
-router.use('/exercise', require('./exercise/exerciseRoutes'));
-router.use('/method', require('./method/methodRoutes'));
-router.use('/attempt', require('./attempt/attemptRoutes'));
 
 module.exports = router;
